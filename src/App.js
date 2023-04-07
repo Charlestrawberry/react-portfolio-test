7fb011c95cc40625c5ce1838b3fc0a9c775811fe
Charlestrawberry <69170563+Charlestrawberry@users.noreply.github.com>
2023-04-06 20:18:57 -0700

    first set-up

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>PROJECT PORTFOLIO</h2>
+    </div>
+  );
+}
+
+export default App;
